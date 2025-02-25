ab3e5e2d31d1d8048ce086320d1efbda0ea695c0
Pedro <pedrodelmonico@hotmail.com>
2025-02-25 20:46:59 -0300

    feat: finished hero section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'backgound-primary': '#050505',
+        'backgound-secondary': '#0f0f10',
+        'backgound-terriary': '#19191a',
+        'content-body': '#cdcbcc',
+        'content-placeholder': '#827d7f',
+        'content-headline': '#b2b2b2',
+        'border-primary': '#19191a',
+        'border-secondary': '#323234',
+        'border-terriary': '#97979b',
+        'accent-purple': '#4b2dbb',
+        'accent-green': '#87bb2d',
+        'accent-pink': '#b5446b',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
